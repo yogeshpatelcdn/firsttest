e90623b2683e0479707485835746739d6f4cd6bf
Yogesh <yogeshpatel@cdnsol.com>
2014-05-06 19:16:08 +0530

    loc

---
+++ geolocationSample/gen/com/example/geolocationsample/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.geolocationsample;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ geolocationSample/src/com/example/geolocationsample/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.geolocationsample;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends /* Activity */DroidGap {
+
+    // @Override
+    // protected void onCreate(Bundle savedInstanceState) {
+    // super.onCreate(savedInstanceState);
+    // setContentView(R.layout.activity_main);
+    // }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/index.html");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
